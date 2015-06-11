68562f1499c868cdf387f18b2e95280b4d3d8645
Thomas <blackdog576@gmail.com>
2015-06-11 12:41:21 +1200

    Started adding GCM code

--- SDHB-client/app/src/main/java/bit/weilytw1/sdhb_client/MainActivity.java
+++ SDHB-client/app/src/main/java/bit/weilytw1/sdhb_client/MainActivity.java
@@ -1,9 +1,14 @@
 package bit.weilytw1.sdhb_client;
 
+import android.app.IntentService;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
 
 
 public class MainActivity extends ActionBarActivity
@@ -14,8 +19,39 @@ public class MainActivity extends ActionBarActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check if Google Play Services installed on users device...
+
+
     }
 
+    public class GCMSetup extends IntentService
+    {
+        /**
+         * Creates an IntentService.  Invoked by your subclass's constructor.
+         *
+         * @param name Used to name the worker thread, important only for debugging.
+         */
+        public GCMSetup(String name) {
+            super(name);
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent)
+        {
+            //Obtain registration token
+            InstanceID instanceID = InstanceID.getInstance(this);
+            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId), GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+        }
+
+        @Override
+        public void onTokenRefresh()
+        {
+            // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
