449410f697ead98f3ceaf210f007c0f454593f71
Thomas <blackdog576@gmail.com>
2015-08-14 15:26:10 +1200

    Working on contacts

--- SDHB-client/app/src/main/java/bit/weilytw1/sdhb_client/ContactsActivity.java
+++ SDHB-client/app/src/main/java/bit/weilytw1/sdhb_client/ContactsActivity.java
@@ -1,23 +1,44 @@
 package bit.weilytw1.sdhb_client;
 
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.Notification;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 import java.lang.reflect.Type;
 
 
 public class ContactsActivity extends TabActivity
 {
+    //Class Properties
+    TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        tabHost = (TabHost)findViewById(android.R.id.tabhost);
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("tabPersonal");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("tabSDHB");
+
+        tab1.setIndicator("Tab1");
+        tab1.setContent(new Intent(this, NotificationsActivity.class));
+        tab2.setIndicator("Tab2");
+        tab2.setContent(new Intent(this, NotificationsActivity.class));
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
     }
 
     @Override
